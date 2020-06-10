75b0c6c2925460b308030f1ba8de92e17270e76d
Hexmagic <191440042@qq.com>
2020-06-10 11:12:15 +0800

    update loss

--- train.py
+++ train.py
@@ -13,47 +13,41 @@ from visdom import Visdom
 
 
 def update_lr(optimizer, epoch):
-    if epoch == 30:
-        lr = 0.001
-    elif epoch == 50:
-        lr = 0.0007
-    elif epoch == 71:
-        lr = 0.0006
-    elif epoch >= 90:
-        lr = 0.0007
-    elif epoch > 110:
-        lr = 0.00005
-    else:
-        return
-
+    lr = 0.001 - (epoch % 5+1) * 0.0001
     for param_group in optimizer.param_groups:
-        param_group['lr'] = lr
+        param_group["lr"] = lr
 
 
 def train():
     dom = Visdom()
-    train_loader = DataLoader(VOCDataset(mode='train'),
-                              batch_size=32,
-                              num_workers=8,
-                              drop_last=True,
-                              pin_memory=True,
-                              shuffle=True)
-    valid_loader = DataLoader(VOCDataset(mode='val'),
-                              batch_size=4,
-                              num_workers=4,
-                              drop_last=True,
-                              shuffle=True)
+    train_loader = DataLoader(
+        VOCDataset(mode="train"),
+        batch_size=32,
+        num_workers=8,
+        drop_last=True,
+        pin_memory=True,
+        shuffle=True,
+    )
+    valid_loader = DataLoader(
+        VOCDataset(mode="val"),
+        batch_size=4,
+        num_workers=4,
+        drop_last=True,
+        shuffle=True,
+    )
     net = yolo().cuda()
-    net = torch.load('weights/75_net.pk')
+    net = torch.load("weights/75_net.pk")
     criterion = YoloLoss().cuda()
-    optim = SGD(params=net.parameters(),
-                lr=0.01,
-                weight_decay=5e-4,
-                momentum=0.9,
-                nesterov=True)
-    #optim = Adam(params=net.parameters(), lr=1e-3, weight_decay=5e-4, eps=1e-4)
-    t_obj_loss,t_nobj_loss,t_xy_loss,t_wh_loss,t_class_loss=[],[],[],[],[]
-    v_obj_loss,v_nobj_loss,v_xy_loss,v_wh_loss,v_class_loss=[],[],[],[],[]
+    optim = SGD(
+        params=net.parameters(),
+        lr=0.001,
+        weight_decay=5e-4,
+        momentum=0.9,
+        nesterov=True,
+    )
+    # optim = Adam(params=net.parameters(), lr=1e-3, weight_decay=5e-4, eps=1e-4)
+    t_obj_loss, t_nobj_loss, t_xy_loss, t_wh_loss, t_class_loss = [], [], [], [], []
+    v_obj_loss, v_nobj_loss, v_xy_loss, v_wh_loss, v_class_loss = [], [], [], [], []
     valid_loss = []
     train_loss = []
 
@@ -69,7 +63,8 @@ def train():
             output = net(img)
             optim.zero_grad()
             obj_loss, noobj_loss, xy_loss, wh_loss, class_loss = criterion(
-                output, target.float())
+                output, target.float()
+            )
             loss = obj_loss + noobj_loss + xy_loss + wh_loss + 2 * class_loss
             loss.backward()
             train_loss.append(loss.item())
@@ -81,23 +76,27 @@ def train():
             optim.step()
             if i % 10 == 0:
                 loss_list = [
-                    np.mean(x) for x in [
-                        t_obj_loss, t_nobj_loss, t_xy_loss, t_wh_loss,
-                        t_class_loss
+                    np.mean(x)
+                    for x in [
+                        t_obj_loss,
+                        t_nobj_loss,
+                        t_xy_loss,
+                        t_wh_loss,
+                        t_class_loss,
                     ]
                 ]
                 train_bar.set_postfix_str(
-                    "o:{:.2f} n:{:.2f} x:{:.2f} w:{:.2f} c:{:.2f}".format(
-                        *loss_list))
-                #train_bar.set_postfix_str(f"loss {np.mean(train_loss)}")
-                dom.line(train_loss, win='train', opts={'title': 'Train loss'})
-                dom.line(t_obj_loss, win='obj', opts={'title': 'obj'})
-                dom.line(t_nobj_loss, win='noobj', opts={'title': 'noobj'})
-                dom.line(t_xy_loss, win='xy', opts={'title': 'xy'})
-                dom.line(t_wh_loss, win='wh', opts={'title': 'wh'})
-                dom.line(t_class_loss, win='class', opts={'title': 'class'})
+                    "o:{:.2f} n:{:.2f} x:{:.2f} w:{:.2f} c:{:.2f}".format(*loss_list)
+                )
+                # train_bar.set_postfix_str(f"loss {np.mean(train_loss)}")
+                dom.line(train_loss, win="train", opts={"title": "Train loss"})
+                dom.line(t_obj_loss, win="obj", opts={"title": "obj"})
+                dom.line(t_nobj_loss, win="noobj", opts={"title": "noobj"})
+                dom.line(t_xy_loss, win="xy", opts={"title": "xy"})
+                dom.line(t_wh_loss, win="wh", opts={"title": "wh"})
+                dom.line(t_class_loss, win="class", opts={"title": "class"})
         if epoch % 5 == 0:
-            torch.save(net, f'weights/{epoch}_net.pk')
+            torch.save(net, f"weights/{epoch}_net.pk")
         net.eval()
         with torch.no_grad():
             for i, ele in enumerate(val_bar):
@@ -105,7 +104,8 @@ def train():
                 img, target = Variable(img).cuda(), Variable(target).cuda()
                 output = net(img)
                 obj_loss, noobj_loss, xy_loss, wh_loss, class_loss = criterion(
-                    output, target.float())
+                    output, target.float()
+                )
                 v_obj_loss.append(obj_loss.item())
                 v_nobj_loss.append(noobj_loss.item())
                 v_xy_loss.append(xy_loss.item())
@@ -115,19 +115,23 @@ def train():
                 valid_loss.append(loss.item())
                 if i % 10 == 0:
                     loss_list = [
-                        np.mean(x) for x in [
-                            v_obj_loss, v_nobj_loss, v_xy_loss, v_wh_loss,
-                            v_class_loss
+                        np.mean(x)
+                        for x in [
+                            v_obj_loss,
+                            v_nobj_loss,
+                            v_xy_loss,
+                            v_wh_loss,
+                            v_class_loss,
                         ]
                     ]
                     val_bar.set_postfix_str(
-                        "o:{:.2f} n:{:.2f} x:{:.2f} w:{:.2f}c:{:.2f}".format(
-                            *loss_list))
-                    dom.line(valid_loss,
-                             win='valid_loss',
-                             opts=dict(title="Valid loss"))
+                        "o:{:.2f} n:{:.2f} x:{:.2f} w:{:.2f}c:{:.2f}".format(*loss_list)
+                    )
+                    dom.line(
+                        valid_loss, win="valid_loss", opts=dict(title="Valid loss")
+                    )
 
-    torch.save(net, f'weights/{epoch}_net.pk')
+    torch.save(net, f"weights/{epoch}_net.pk")
 
 
 def test():
--- util/loss.py
+++ util/loss.py
@@ -34,9 +34,9 @@ class YoloLoss(Module):
         inter_w = (w1 + w2) - (max(x1 + w1, x2 + w2) - min(x1, x2))
         inter_h = (h1 + h2) - (max(y1 + h1, y2 + h2) - min(y1, y2))
 
-        if inter_h <= 0 or inter_w <= 0:  #代表相交区域面积为0
+        if inter_h <= 0 or inter_w <= 0:  # 代表相交区域面积为0
             return 0
-        #往下进行应该inter 和 union都是正值
+        # 往下进行应该inter 和 union都是正值
         inter = inter_w * inter_h
         union = w1 * h1 + w2 * h2 - inter
         return (inter / union).item()
@@ -44,6 +44,7 @@ class YoloLoss(Module):
     def forward(self, pred, target):
         batch_size = pred.size(0)
         mask = target[:, :, :, 4] > 0
+        noobj_mask = target[:, :, :, 4] == 0
         target_cell = target[mask]
         pred_cell = pred[mask]
         obj_loss = 0
@@ -53,39 +54,32 @@ class YoloLoss(Module):
             box = target_cell[i][:4]
             index = indexs[i][1:]
             pbox1, pbox2 = pred_cell[i][:4], pred_cell[i][5:9]
-            iou1, iou2 = self.compute_iou(box, pbox1, index), self.compute_iou(
-                box, pbox2, index)
+            iou1, iou2 = (
+                self.compute_iou(box, pbox1, index),
+                self.compute_iou(box, pbox2, index),
+            )
             if iou1 > iou2:
                 target_cell[i][4] = iou1
+                target_cell[i][9] = 0
             else:
-                target_cell[i][4] = iou2
-                pred_cell[i][:4] = pbox2
-                pred_cell[i][4] = pred_cell[i][9]
-
-        noobj_mask = target[:, :, :, 4] == 0
+                target_cell[i][9] = iou2
+                target_cell[i][4] = 0
         noobj_pred = pred[noobj_mask][:, :10].contiguous().view(-1, 5)
         noobj_target = target[noobj_mask][:, :10].contiguous().view(-1, 5)
+        tc = target_cell.view(-1,5)
+        pc = pred_cell.view(-1,5)
+        noobj_loss = F.mse_loss(noobj_target[:, 4], noobj_pred[:, 4], reduction="sum")
+        obj_loss = F.mse_loss(tc[:, 4], pc[:, 4], reduction="sum")
+        xy_loss = F.mse_loss(pred_cell[:, :2], target_cell[:, :2], reduction="sum")
+        wh_loss = F.mse_loss(pred_cell[:, 2:4], target_cell[:, 2:4], reduction="sum")
 
-        noobj_loss = F.mse_loss(noobj_target[:, 4],
-                                noobj_pred[:, 4],
-                                reduction='sum')
-        obj_loss = F.mse_loss(pred_cell[:, 4],
-                              target_cell[:, 4],
-                              reduction='sum')
-        xy_loss = F.mse_loss(pred_cell[:, :2],
-                             target_cell[:, :2],
-                             reduction='sum')
-        wh_loss = F.mse_loss(pred_cell[:, 2:4],
-                             target_cell[:, 2:4],
-                             reduction='sum')
-
-        class_loss = F.mse_loss(pred_cell[:, 10:],
-                                target_cell[:, 10:],
-                                reduction='sum')
+        class_loss = F.mse_loss(pred_cell[:, 10:], target_cell[:, 10:], reduction="sum")
         loss = [
-            obj_loss, self.lambda_noobj * noobj_loss,
-            self.lambda_coord * xy_loss, self.lambda_coord * wh_loss,
-            class_loss
+            obj_loss,
+            self.lambda_noobj * noobj_loss,
+            self.lambda_coord * xy_loss,
+            self.lambda_coord * wh_loss,
+            class_loss,
         ]
         loss = [ele / batch_size for ele in loss]
         return loss
