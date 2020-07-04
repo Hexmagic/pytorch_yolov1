382bc2a13e7c9ce1896a7b7ddd275e3fb15e9b40
Hexmagic <191440042@qq.com>
2020-07-05 00:33:20 +0800

    更新loss和model

--- model/loss.py
+++ model/loss.py
@@ -1,97 +1,143 @@
-import numpy as np
 import torch
+import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
-from torch.nn import *
 
 
-class YoloLoss(Module):
-    def __init__(self, num_class=20):
-        super(YoloLoss, self).__init__()
-        self.lambda_coord = 5
-        self.lambda_noobj = 0.5
-        self.S = 7
-        self.B = 2
-        self.C = num_class
+class yoloLoss(nn.Module):
+    def __init__(self, S, B, l_coord, l_noobj):
+        super(yoloLoss, self).__init__()
+        self.S = S
+        self.B = B
+        self.l_coord = l_coord
+        self.l_noobj = l_noobj
 
-    def conver_box(self, box, index):
-        x, y, w, h = box
-        i, j = index
-        step = 1 / self.S
-        x = (x + j) * step
-        y = (y + i) * step
-        # x, y, w, h = x.item(), y.item(), w.item(), h.item()
-        a, b, c, d = [x - w / 2, y - h / 2, x + w / 2, y + h / 2]
-        return [max(a.item(), 0), max(b.item(), 0), w, h]
+    def compute_iou(self, box1, box2):
+        '''Compute the intersection over union of two set of boxes, each box is [x1,y1,x2,y2].
+        Args:
+          box1: (tensor) bounding boxes, sized [N,4].
+          box2: (tensor) bounding boxes, sized [M,4].
+        Return:
+          (tensor) iou, sized [N,M].
+        '''
+        N = box1.size(0)
+        M = box2.size(0)
 
-    def compute_iou(self, box1, box2, index):
-        box1 = self.conver_box(box1, index)
-        box2 = self.conver_box(box2, index)
-        x1, y1, w1, h1 = box1
-        x2, y2, w2, h2 = box2
-        # 获取相交
-        inter_w = (w1 + w2) - (max(x1 + w1, x2 + w2) - min(x1, x2))
-        inter_h = (h1 + h2) - (max(y1 + h1, y2 + h2) - min(y1, y2))
+        lt = torch.max(
+            box1[:, :2].unsqueeze(1).expand(N, M,
+                                            2),  # [N,2] -> [N,1,2] -> [N,M,2]
+            box2[:, :2].unsqueeze(0).expand(N, M,
+                                            2),  # [M,2] -> [1,M,2] -> [N,M,2]
+        )
 
-        if inter_h <= 0 or inter_w <= 0:  # 代表相交区域面积为0
-            return 0
-        # 往下进行应该inter 和 union都是正值
-        inter = inter_w * inter_h
-        union = w1 * h1 + w2 * h2 - inter
-        return (inter / union).item()
+        rb = torch.min(
+            box1[:, 2:].unsqueeze(1).expand(N, M,
+                                            2),  # [N,2] -> [N,1,2] -> [N,M,2]
+            box2[:, 2:].unsqueeze(0).expand(N, M,
+                                            2),  # [M,2] -> [1,M,2] -> [N,M,2]
+        )
 
-    def forward(self, pred, target):
-        batch_size = pred.size(0)
-        mask = target[:, :, :, 4] > 0
-        noobj_mask = target[:, :, :, 4] == 0
-        target_cell = target[mask]
-        pred_cell = pred[mask]
-        obj_loss = 0
-        arry = mask.cpu().numpy()
-        indexs = np.argwhere(arry == True)
-        for i in range(len(target_cell)):
-            box = target_cell[i][:4]
-            index = indexs[i][1:]
-            pbox1, pbox2 = pred_cell[i][:4], pred_cell[i][5:9]
-            iou1, iou2 = (
-                self.compute_iou(box, pbox1, index),
-                self.compute_iou(box, pbox2, index),
-            )
-            if iou1 > iou2:
-                target_cell[i][4] = iou1
-                target_cell[i][9] = 0
-                pred_cell[i][9] = 0
-            else:
-                target_cell[i][9] = iou2
-                target_cell[i][4] = 0
-                pred_cell[i][4] = 0
+        wh = rb - lt  # [N,M,2]
+        wh[wh < 0] = 0  # clip at 0
+        inter = wh[:, :, 0] * wh[:, :, 1]  # [N,M]
 
-        tc = target_cell[..., :10].contiguous().view(-1, 5)
-        pc = pred_cell[..., :10].contiguous().view(-1, 5)
+        area1 = (box1[:, 2] - box1[:, 0]) * (box1[:, 3] - box1[:, 1])  # [N,]
+        area2 = (box2[:, 2] - box2[:, 0]) * (box2[:, 3] - box2[:, 1])  # [M,]
+        area1 = area1.unsqueeze(1).expand_as(inter)  # [N,] -> [N,1] -> [N,M]
+        area2 = area2.unsqueeze(0).expand_as(inter)  # [M,] -> [1,M] -> [N,M]
 
-        # noobj_mask = tc[..., 4] == 0
-        noobj_pred = pred[:, :10][noobj_mask].contiguous().view(-1, 5)
-        noobj_target = target[:, :10][noobj_mask].contiguous().view(-1, 5)
+        iou = inter / (area1 + area2 - inter)
+        return iou
 
-        mask = tc[..., 4] != 0
-        tc = tc[mask]
-        pc = pc[mask]
+    def forward(self, pred_tensor, target_tensor):
+        '''
+        pred_tensor: (tensor) size(batchsize,S,S,Bx5+20=30) [x,y,w,h,c]
+        target_tensor: (tensor) size(batchsize,S,S,30)
+        '''
+        N = pred_tensor.size()[0]
+        coo_mask = target_tensor[:, :, :, 4] > 0
+        noo_mask = target_tensor[:, :, :, 4] == 0
+        coo_mask = coo_mask.unsqueeze(-1).expand_as(target_tensor)
+        noo_mask = noo_mask.unsqueeze(-1).expand_as(target_tensor)
 
-        noobj_loss = F.mse_loss(noobj_target[:, 4],
-                                noobj_pred[:, 4],
-                                reduction="sum")
-        obj_loss = F.mse_loss(tc[:, 4], pc[:, 4], reduction="sum")
-        xy_loss = F.mse_loss(tc[:, :2], pc[:, :2], reduction="sum")
-        wh_loss = F.mse_loss(torch.sqrt(tc[:, 2:4]),
-                             torch.sqrt(pc[:, 2:4]),
-                             reduction="sum")
+        coo_pred = pred_tensor[coo_mask].view(-1, 30)
+        box_pred = coo_pred[:, :10].contiguous().view(-1,
+                                                      5)  #box[x1,y1,w1,h1,c1]
+        class_pred = coo_pred[:, 10:]  #[x2,y2,w2,h2,c2]
 
-        class_loss = F.mse_loss(pred_cell[:, 10:],
-                                target_cell[:, 10:],
-                                reduction="sum")
-        loss = dict(conf_loss=(obj_loss + self.lambda_noobj * noobj_loss) /
-                    batch_size,
-                    reg_loss=(self.lambda_coord * xy_loss +
-                              self.lambda_coord * wh_loss) / batch_size,
-                    cls_loss=class_loss / batch_size)
-        return loss
+        coo_target = target_tensor[coo_mask].view(-1, 30)
+        box_target = coo_target[:, :10].contiguous().view(-1, 5)
+        class_target = coo_target[:, 10:]
+
+        # compute not contain obj loss
+        noo_pred = pred_tensor[noo_mask].view(-1, 30)
+        noo_target = target_tensor[noo_mask].view(-1, 30)
+        noo_pred_mask = torch.cuda.ByteTensor(noo_pred.size())
+        noo_pred_mask.zero_()
+        noo_pred_mask[:, 4] = 1
+        noo_pred_mask[:, 9] = 1
+        noo_pred_c = noo_pred[noo_pred_mask]  #noo pred只需要计算 c 的损失 size[-1,2]
+        noo_target_c = noo_target[noo_pred_mask]
+        nooobj_loss = F.mse_loss(noo_pred_c, noo_target_c, size_average=False)
+
+        #compute contain obj loss
+        coo_response_mask = torch.cuda.ByteTensor(box_target.size())
+        coo_response_mask.zero_()
+        coo_not_response_mask = torch.cuda.ByteTensor(box_target.size())
+        coo_not_response_mask.zero_()
+        box_target_iou = torch.zeros(box_target.size()).cuda()
+        for i in range(0, box_target.size()[0], 2):  #choose the best iou box
+            box1 = box_pred[i:i + 2]
+            box1_xyxy = Variable(torch.FloatTensor(box1.size()))
+            box1_xyxy[:, :2] = box1[:, :2] / 14. - 0.5 * box1[:, 2:4]
+            box1_xyxy[:, 2:4] = box1[:, :2] / 14. + 0.5 * box1[:, 2:4]
+            box2 = box_target[i].view(-1, 5)
+            box2_xyxy = Variable(torch.FloatTensor(box2.size()))
+            box2_xyxy[:, :2] = box2[:, :2] / 14. - 0.5 * box2[:, 2:4]
+            box2_xyxy[:, 2:4] = box2[:, :2] / 14. + 0.5 * box2[:, 2:4]
+            iou = self.compute_iou(box1_xyxy[:, :4], box2_xyxy[:, :4])  #[2,1]
+            max_iou, max_index = iou.max(0)
+            max_index = max_index.data.cuda()
+
+            coo_response_mask[i + max_index] = 1
+            coo_not_response_mask[i + 1 - max_index] = 1
+
+            #####
+            # we want the confidence score to equal the
+            # intersection over union (IOU) between the predicted box
+            # and the ground truth
+            #####
+            box_target_iou[i + max_index,
+                           torch.LongTensor([4]).cuda()] = (
+                               max_iou).data.cuda()
+        box_target_iou = Variable(box_target_iou).cuda()
+        #1.response loss
+        box_pred_response = box_pred[coo_response_mask].view(-1, 5)
+        box_target_response_iou = box_target_iou[coo_response_mask].view(-1, 5)
+        box_target_response = box_target[coo_response_mask].view(-1, 5)
+        contain_loss = F.mse_loss(box_pred_response[:, 4],
+                                  box_target_response_iou[:, 4],
+                                  size_average=False)
+        loc_loss = F.mse_loss(box_pred_response[:, :2],
+                              box_target_response[:, :2],
+                              size_average=False) + F.mse_loss(
+                                  torch.sqrt(box_pred_response[:, 2:4]),
+                                  torch.sqrt(box_target_response[:, 2:4]),
+                                  size_average=False)
+        #2.not response loss
+        box_pred_not_response = box_pred[coo_not_response_mask].view(-1, 5)
+        box_target_not_response = box_target[coo_not_response_mask].view(-1, 5)
+        box_target_not_response[:, 4] = 0
+        #not_contain_loss = F.mse_loss(box_pred_response[:,4],box_target_response[:,4],size_average=False)
+
+        #I believe this bug is simply a typo
+        not_contain_loss = F.mse_loss(box_pred_not_response[:, 4],
+                                      box_target_not_response[:, 4],
+                                      size_average=False)
+
+        #3.class loss
+        class_loss = F.mse_loss(class_pred, class_target, size_average=False)
+        return dict(conf_loss=(2 * contain_loss + not_contain_loss +
+                               self.l_noobj * nooobj_loss) / N,
+                    reg_loss=selef.l_coord * loc_loss / N,
+                    cls_loss=class_loss / N)
--- model/yolo.py
+++ model/yolo.py
@@ -1,15 +1,30 @@
 from torch import nn
 from model.vgg import build_vgg
 import torch
-
+import math
 
 class YoLo(nn.Module):
     def __init__(self, features, num_classes=20):
         super(YoLo, self).__init__()
         self.features = features
         self.classify = nn.Sequential(nn.Linear(512 * 7 * 7, 4096),
-                                      nn.LeakyReLU(inplace=True),
+                                      nn.PReLU(inplace=True),
+                                      nn.Dropout(),
                                       nn.Linear(4096, 1470))
+
+    def init_param(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+                if m.bias is not None:
+                    m.bias.data.zero_()
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+            elif isinstance(m, nn.Linear):
+                m.weight.data.normal_(0, 0.01)
+                m.bias.data.zero_()
 
     def forward(self, x):
         x = self.features(x)
